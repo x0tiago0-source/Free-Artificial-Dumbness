e7fe3d97e9d74df8201b465c6d2d6faf5a722862
Tiago <x0tiago0@gmail.com>
2026-09-18 16:22:24 +0100

    Update settings.gradle.kts

--- free-artificial-dumbness/free-artificial-dumbness/gradle/settings.gradle.kts
+++ free-artificial-dumbness/free-artificial-dumbness/gradle/settings.gradle.kts
@@ -1,1 +1,17 @@
-// TODO: Configurar settings.gradle.kts
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "Free-Artificial-Dumbness"
+include(":app") // Inclui o módulo principal da aplicação
